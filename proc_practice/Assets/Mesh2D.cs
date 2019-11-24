17a4f965f27a3eefe5798421b27d08f3dc0da0fa
ashrafuz <rawnok02@gmail.com>
2019-11-24 13:18:41 +0600

    basic road done without texture

---
+++ proc_practice/Assets/Mesh2D.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Mesh2D : ScriptableObject {
+
+    [System.Serializable]
+    public struct Vertex {
+        public Vector2 points;
+        public Vector2 normals;
+        [HideInInspector] public float onlyUs; /// uvs, but like v : happy face
+    }
+
+    public Vertex[] vertices;
+    public int[] lineIndices;
+
+    public int GetVertexCount () {
+        return vertices.Length;
+    }
+
+    public int GetLineCount () {
+        return lineIndices.Length;
+    }
+}
--- proc_practice/Assets/RoadSegment.cs
+++ proc_practice/Assets/RoadSegment.cs
@@ -1,15 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
 public struct OrientedPoint {
     public Quaternion rotation;
     public Vector3 position;
+
+    public Vector3 LocalToWorld (Vector3 _localSpace) {
+        return position + rotation * _localSpace;
+    }
+
+    public Vector3 LocalToWorldVec (Vector3 _localSpace) {
+        return rotation * _localSpace;
+    }
 }
 
 public class RoadSegment : MonoBehaviour {
 
+    [Range (2, 32)][SerializeField] int m_VerticalSliceCount = 8;
     [Range (1, 5)][SerializeField] float m_CurveThickness = 1;
     [Range (0, 1)][SerializeField] float m_CurvePointPercent = 1;
     [Range (3, 128)][SerializeField] int m_CurveDetailLevel = 3;
@@ -18,6 +28,7 @@ public class RoadSegment : MonoBehaviour {
     int VertexCount => m_CurveDetailLevel * 2;
 
     [SerializeField] MeshFilter m_MeshFilter;
+    [SerializeField] Mesh2D m_Shape2D;
     [SerializeField] Transform[] points = new Transform[4];
 
     Vector3 GetPos (int i) => points[i].position;
@@ -35,19 +46,84 @@ public class RoadSegment : MonoBehaviour {
             GetPos (2), // end tangent
             Color.white, EditorGUIUtility.whiteTexture, m_CurveThickness);
 
-        Gizmos.color = Color.green;
+        Gizmos.color = Color.magenta;
 
         OrientedPoint percentPoint = GetBezierOrientation (m_CurvePointPercent);
         Gizmos.DrawSphere (percentPoint.position, 0.03f);
         Handles.PositionHandle (percentPoint.position, percentPoint.rotation);
 
-        Gizmos.color = Color.red;
+        //void DrawPoint (Vector2 localPos) => Gizmos.DrawSphere (percentPoint.LocalToWorld (localPos), 0.1f);
 
+        Vector3[] verts = m_Shape2D.vertices.Select (v => percentPoint.LocalToWorld (v.points)).ToArray ();
+
+        for (int i = 0; i < m_Shape2D.lineIndices.Length; i += 2) {
+            Vector3 a = verts[m_Shape2D.lineIndices[i]];
+            Vector3 b = verts[m_Shape2D.lineIndices[i + 1]];
+
+            Gizmos.DrawLine (a, b);
+        }
+
+        Gizmos.color = Color.red;
         GenerateRoad ();
     }
 
     private Mesh mMesh;
     private void GenerateRoad () {
+        if (mMesh == null) {
+            mMesh = new Mesh ();
+            mMesh.name = "RoadSegment";
+            m_MeshFilter.sharedMesh = mMesh;
+        }
+
+        mMesh.Clear ();
+
+        //vertices
+        List<Vector3> allVertsAlongRoad = new List<Vector3> ();
+        List<Vector3> allNormals = new List<Vector3> ();
+
+        for (int ring = 0; ring < m_VerticalSliceCount; ring++) {
+            float t = ring / (m_VerticalSliceCount - 1f);
+
+            OrientedPoint op = GetBezierOrientation (t);
+            for (int i = 0; i < m_Shape2D.vertices.Length; i++) {
+                allVertsAlongRoad.Add (op.LocalToWorld (m_Shape2D.vertices[i].points));
+                allNormals.Add (op.LocalToWorldVec (m_Shape2D.vertices[i].normals));
+                //allNormals.Add (m_Shape2D.vertices[i].normals);//without rotation
+
+            }
+        }
+
+        //Trianlges
+        List<int> allTriangles = new List<int> ();
+        for (int ring = 0; ring < m_VerticalSliceCount - 1; ring++) {
+            int rootIndex = ring * m_Shape2D.GetVertexCount ();
+            int rootIndexForward = (ring + 1) * m_Shape2D.GetVertexCount ();
+
+            for (int line = 0; line < m_Shape2D.GetLineCount (); line += 2) {
+                int lineA = m_Shape2D.lineIndices[line];
+                int lineB = m_Shape2D.lineIndices[line + 1];
+
+                int currentA = rootIndex + lineA;
+                int currentB = rootIndex + lineB;
+                int nextA = rootIndexForward + lineA;
+                int nextB = rootIndexForward + lineB;
+
+                allTriangles.Add (currentA);
+                allTriangles.Add (nextA);
+                allTriangles.Add (nextB);
+
+                allTriangles.Add (nextB);
+                allTriangles.Add (currentB);
+                allTriangles.Add (currentA);
+            }
+        }
+        mMesh.SetVertices (allVertsAlongRoad);
+        mMesh.SetTriangles (allTriangles, 0);
+        mMesh.SetNormals (allNormals);
+
+    }
+
+    private void GeneratePitch () {
         if (mMesh == null) {
             mMesh = new Mesh ();
             mMesh.name = "Road Segment";
@@ -65,8 +141,8 @@ public class RoadSegment : MonoBehaviour {
 
             OrientedPoint bezierPoint = GetBezierOrientation (t);
 
-            Vector3 localLeftVector = (bezierPoint.position) + (Vector3.left * m_RoadThickness * 0.5f);
-            Vector3 localRightVector = (bezierPoint.position) + (Vector3.right * m_RoadThickness * 0.5f);
+            Vector3 localLeftVector = bezierPoint.LocalToWorld (-Vector3.right * (m_CurveThickness * 0.1f)); //(bezierPoint.position) + (Vector3.left * m_RoadThickness * 0.5f);
+            Vector3 localRightVector = bezierPoint.LocalToWorld (Vector3.right * (m_CurveThickness * 0.1f)); //(bezierPoint.position) + (Vector3.right * (m_CurveThickness * 0.1f));
 
             vertices.Add (localLeftVector);
             normals.Add (-Vector3.up); // in localspace
@@ -74,8 +150,8 @@ public class RoadSegment : MonoBehaviour {
             vertices.Add (localRightVector);
             normals.Add (-Vector3.up); // in localspace
 
-            Gizmos.DrawSphere (localLeftVector, 0.01f);
-            Gizmos.DrawSphere (localRightVector, 0.01f);
+            Gizmos.DrawSphere (localLeftVector, 0.005f);
+            Gizmos.DrawSphere (localRightVector, 0.005f);
 
             uvs.Add (new Vector2 (1, t));
             uvs.Add (new Vector2 (0, t));
@@ -89,12 +165,12 @@ public class RoadSegment : MonoBehaviour {
             int rtOuterNext = (rootIndex + 2);
             int rtInnerNext = (rootIndex + 3);
 
-            triangleIndices.Add (rtOuterNext);
             triangleIndices.Add (rootIndex);
+            triangleIndices.Add (rtOuterNext);
             triangleIndices.Add (rtInnerNext);
 
-            triangleIndices.Add (rtInnerNext);
             triangleIndices.Add (rootIndex);
+            triangleIndices.Add (rtInnerNext);
             triangleIndices.Add (rtInner);
         }
 
